f87ffae8291b6d274867fb6af1d38d7d834b34e9
Kelvin-T-Lu <61221557+Kelvin-T-Lu@users.noreply.github.com>
2022-02-16 15:22:18 -0500

    Feature Alterations

--- hw1_klu21.py
+++ hw1_klu21.py
@@ -76,15 +76,15 @@ def stem_helper(text):
     tokenized_words = word_tokenize(text)
     stop_words = stopwords.words('english')
     
-    # stemmer = SnowballStemmer(language='english')
+    stemmer = SnowballStemmer(language='english')
     # stemmer = PorterStemmer() 
     # stemmer = LancasterStemmer()
-    lemmatizer = WordNetLemmatizer()
+    # lemmatizer = WordNetLemmatizer()
     # print(snow_stemmer.stem('managed'))
     
 
-    # tokenized_words = [stemmer.stem(word) for word in tokenized_words]
-    tokenized_words = [lemmatizer.lemmatize(word) for word in tokenized_words]
+    tokenized_words = [stemmer.stem(word) for word in tokenized_words if word not in stop_words]
+    # tokenized_words = [lemmatizer.lemmatize(word) for word in tokenized_words]
     # if word not in stop_words
     return tokenized_words
     # return text
@@ -117,8 +117,9 @@ class knn_classifer():
         # print(self.training[0])
 
         print('Distance calculating...')
+        
         # method = 1
-        # distances = cosine_similarity(self.training, x_test)
+        # distances = cosine_similarity(x_test, self.training)
         method = 0 
         distances = euclidean_distances(x_test, self.training)
 
@@ -133,7 +134,7 @@ class knn_classifer():
     def generate_predictions(self, dist_matrix, flag):
         # Return ordered list (closest to furthest) of k neighbors
 
-        print('Genreating predictions')
+        print('Generating predictions')
         output = np.zeros(dist_matrix.shape[0], dtype = 'int8')
 
         # print(dist_matrix)
@@ -145,7 +146,7 @@ class knn_classifer():
 
             # Cosine-Similarity - Reverse the row 
             if(flag == 1):
-                print('Cosine List Reversed')
+                # print('Cosine List Reversed')
                 sorted_row = np.flip(sorted_row)
 
             # Find the first k neighbor from the sorted_row. 
@@ -169,7 +170,8 @@ def write_helper(num):
 
 def run_K(train_data, training_vectors, test_tfidf):
     # k_neighbors = 10
-    k_neighbors = math.floor(math.sqrt(train_data.shape[0]))
+    # k_neighbors = math.floor(math.sqrt(train_data.shape[0]))
+    k_neighbors = 91
     if(k_neighbors % 2 == 0):
         k_neighbors = k_neighbors + 1 
 
@@ -182,9 +184,30 @@ def run_K(train_data, training_vectors, test_tfidf):
 
     # print(test_data.head())
 
+def k_tests(training_vectors, train_data):
+    k_range = range(77,99,2)
+    # k_range = [3,25,51,75,101,123]
+    print('Running tests ... ')
+    # k_range = [51]
+    k_scores = []
+    for k in k_range:
+        # build classifier here
+        knn = KNeighborsClassifier(n_neighbors = k)
+        scores = cross_val_score(knn, training_vectors, train_data['Ratings'] ,cv=5,scoring='accuracy')
+        print ('k:',k,' mean:',scores.mean(), ' std:', scores.std())
+        k_scores.append(scores.mean()) 
+    
+    plt.plot(k_range, k_scores)
+    plt.xlabel('Value of K for KNN')
+    plt.ylabel('Cross-Validated Accuracy')
+    plt.plot()
+
+
 def main():
     # Entry main function
     # Read the input training data and give it a rating.
+    # Pre-process training and test data. 
+    # Calc the K
 
     start = time.time()
 
@@ -201,36 +224,22 @@ def main():
 
     print('Proprocessing ...')
     train_data['Reviews'] = train_data['Reviews'].apply(lambda x: preprocess_helper(x))
-    test_data['Reviews'] = test_data['Reviews'].apply(lambda x: preprocess_helper(x))
+    # test_data['Reviews'] = test_data['Reviews'].apply(lambda x: preprocess_helper(x))
 
     # print(train_data.head(50))
 
     #  max_df = 0.70
     # min_df=50, max_df = 0.70,
     print('Vectorizing Training Data ...')
-    vectorizer = TfidfVectorizer(max_features = 10000, tokenizer = stem_helper, lowercase = False)
+    vectorizer = TfidfVectorizer(max_df = 0.85, max_features = 10000, tokenizer = stem_helper, lowercase = False)
     training_vectors = vectorizer.fit_transform(train_data['Reviews'])
     test_tfidf = vectorizer.transform(test_data['Reviews'])
 
     print('Training vectors dimensions - ', training_vectors.shape)
     print('Test vectors dimensions - ', test_tfidf.shape)
-    # print(vectorizer.vocabulary_)
+    # print(vectorizer.vocabulary   _)
 
-    # k_range = range(1,k_neighbors)
-    # k_range = [3,25,51,75,101,123]
-    # k_scores = []
-    # for k in k_range:
-        # build classifier here
-        # knn = KNeighborsClassifier(n_neighbors = k)
-        # scores = cross_val_score(knn, training_vectors, train_data['Ratings'] ,cv=5,scoring='accuracy')
-        # print ('k:',k,' mean:',scores.mean(), ' std:', scores.std())
-        # k_scores.append(scores.mean()) 
-    
-    # plt.plot(k_range, k_scores)
-    # plt.xlabel('Value of K for KNN')
-    # plt.ylabel('Cross-Validated Accuracy')
-    # plt.plot()
-
+    # k_tests(training_vectors, train_data)
 
     # Run the classifer 
     test_data['Ratings'] = run_K(train_data, training_vectors, test_tfidf)
